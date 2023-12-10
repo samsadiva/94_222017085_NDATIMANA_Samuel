14843a3cd73fac61d5234f2447a16495396627cf
samsadiva <139139311+samsadiva@users.noreply.github.com>
2023-12-10 20:09:53 +0200

    JAVA WITH MYSQL USE MYSQL-CONNECTOR : STOCK MANAGEMENT SYSTEM :+1:

--- add_number.java
+++ add_number.java
@@ -1,9 +1,9 @@
-public class Q16 {
+class Q16 {
     public static void main(String args[]) {
-        short a = 10;
-        short b = 10;
-        // a+=b;//a=a+b internally so fine
-        a = a + b;// Compile time error because 10+10=20 now int
+        int a = 10;
+        int b = 10;
+        a+=b;//a=a+b internally so fine
+    
         System.out.println(a);
     }
 
